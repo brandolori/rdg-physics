0b12512e268b9a7b0089a7e5fb68f5eb94fe4ede
lorib <lori.barto@live.it>
2021-05-22 12:25:24 +0200

    level 1 work

--- assets/script/WorldComposer.ts
+++ assets/script/WorldComposer.ts
@@ -9,9 +9,20 @@ export default class NewClass extends cc.Component {
     @property
     collisionGroupName = "Collisions"
 
+    @property
+    coinsGroupName = "Coins"
+
+    @property
+    staticObstaclesGroupName = "StaticObstacles"
+
     @property(cc.Prefab)
     collisionPrefab: cc.Prefab = null
 
+    @property(cc.Prefab)
+    coinsPrefab: cc.Prefab = null
+
+    @property(cc.Prefab)
+    staticObstaclesPrefab: cc.Prefab = null
 
     onLoad() {
         const collisionObjects = this.worldMap.getObjectGroup(this.collisionGroupName).getObjects()
@@ -24,14 +35,37 @@ export default class NewClass extends cc.Component {
             node.x = obj.x
             node.y = obj.y - obj.height
 
-            node.anchorX = .5
-            node.anchorY = .5
-
             const collider = node.getComponent(cc.PhysicsBoxCollider)
 
             collider.size = new cc.Size(obj.width, obj.height)
             collider.offset = new cc.Vec2(obj.width / 2, obj.height / 2)
             this.node.addChild(node)
+        })
+
+        const coinsObjects = this.worldMap.getObjectGroup(this.coinsGroupName).getObjects()
+
+        coinsObjects.forEach(obj => {
+            const node = cc.instantiate(this.coinsPrefab)
+
+            node.x = obj.x + obj.width / 2
+            node.y = obj.y - obj.height / 2
+
+            this.node.addChild(node)
+
+            node.scale = 1 / this.node.scale
+        })
+
+        const staticObstaclesObjects = this.worldMap.getObjectGroup(this.staticObstaclesGroupName).getObjects()
+
+        staticObstaclesObjects.forEach(obj => {
+            const node = cc.instantiate(this.staticObstaclesPrefab)
+
+            node.x = obj.x + obj.width / 2
+            node.y = obj.y - obj.height / 2
+
+            this.node.addChild(node)
+
+            node.scale = 1 / this.node.scale
         })
 
     }

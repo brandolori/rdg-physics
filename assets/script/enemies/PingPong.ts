5788a826e6539446153ec4403ba7fbd8c058892f
lorib <lori.barto@live.it>
2021-05-26 13:18:14 +0200

    new moving enemy script

--- assets/script/enemies/PingPong.ts
+++ assets/script/enemies/PingPong.ts
@@ -1,18 +1,17 @@
-import Game from "./Game";
+import Game from "../Game";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class LinearMovement extends cc.Component {
-
-    @property(cc.Node)
-    nodeA: cc.Node = null
+export default class PingPong extends cc.Component {
 
     @property(cc.Node)
     nodeB: cc.Node = null
 
-    @property(cc.Node)
-    vessel: cc.Node = null
+    @property(cc.Prefab)
+    prefab: cc.Prefab = null
+
+    vessel: cc.Node
 
     @property
     speed = 100
@@ -24,9 +23,14 @@ export default class LinearMovement extends cc.Component {
 
     progress = 0
 
+    posA = cc.Vec3.ZERO
+
 
     start() {
-        this.duration = this.nodeA.position.sub(this.nodeB.position).mag() / this.speed
+        this.duration = this.posA.sub(this.nodeB.position).mag() / this.speed
+
+        this.vessel = cc.instantiate(this.prefab)
+        this.vessel.parent = this.node
     }
 
     update(dt: number) {
@@ -36,12 +40,12 @@ export default class LinearMovement extends cc.Component {
         this.progress = (this.progress + dt / this.duration) % 1
 
         const x = Math.abs(1 - this.progress * 2)
-        
-        this.vessel.x = cc.misc.lerp(this.nodeA.x, this.nodeB.x, x)
-        this.vessel.y = cc.misc.lerp(this.nodeA.y, this.nodeB.y, x)
+        this.vessel.x = cc.misc.lerp(this.posA.x, this.nodeB.x, x)
+        this.vessel.y = cc.misc.lerp(this.posA.y, this.nodeB.y, x)
 
         if (this.flip)
             this.vessel.scaleX = this.progress < .5 ? -1 : 1
+
     }
 
 }
